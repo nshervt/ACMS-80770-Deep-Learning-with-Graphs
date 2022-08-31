49b3d9582efc62b4d54fe688c890a74ca30bbd0a
nshervt <navidst@colorado.edu>
2022-08-31 17:46:29 -0400

    ENH: HW starter code added

---
+++ Homework 1/HW1.py
@@ -0,0 +1,56 @@
+"""
+ACMS 80770-03: Deep Learning with Graphs
+Instructor: Navid Shervani-Tabar
+Fall 2022
+University of Notre Dame
+
+Homework 1: Programming assignment
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from networkx.algorithms import bipartite
+from networkx.generators.random_graphs import erdos_renyi_graph
+import copy
+
+
+# -- Initialize graphs
+seed = 30
+G = nx.florentine_families_graph()
+nodes = G.nodes()
+
+layout = nx.spring_layout(G, seed=seed)
+
+# -- compute jaccard's similarity
+"""
+    This example is using NetwrokX's native implementation to compute similarities.
+    Write a code to compute Jaccard's similarity and replace with this function.
+"""
+pred = nx.jaccard_coefficient(G)
+
+
+# -- keep a copy of edges in the graph
+old_edges = copy.deepcopy(G.edges())
+
+# -- add new edges representing similarities.
+new_edges, metric = [], []
+for u, v, p in pred:
+    G.add_edge(u, v)
+    print(f"({u}, {v}) -> {p:.8f}")
+    new_edges.append((u, v))
+    metric.append(p)
+
+# -- plot Florentine Families graph
+nx.draw_networkx_nodes(G, nodelist=nodes, label=nodes, pos=layout, node_size=600)
+nx.draw_networkx_edges(G, edgelist=old_edges, pos=layout, edge_color='gray', width=4)
+
+# -- plot edges representing similarity
+"""
+    This example is randomly plotting similarities between 8 pairs of nodes in the graph. 
+    Identify the ”Ginori”
+"""
+ne = nx.draw_networkx_edges(G, edgelist=new_edges[:8], pos=layout, edge_color=np.asarray(metric[:8]), width=4, alpha=0.7)
+plt.colorbar(ne)
+plt.axis('off')
+plt.show()
