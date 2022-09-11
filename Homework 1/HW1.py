b7b2b7bd87379b12e25ce7ea48a10a0f1f634df6
Priyesh Kakka <39474544+kakkapriyesh@users.noreply.github.com>
2022-09-11 18:53:54 -0400

    Add files via upload

--- Homework 1/HW1.py
+++ Homework 1/HW1.py
@@ -5,6 +5,8 @@ Fall 2022
 University of Notre Dame
 
 Homework 1: Programming assignment
+
+Edited : Priyesh Rajesh Kakka
 """
 
 import networkx as nx
@@ -13,44 +15,85 @@ import numpy as np
 from networkx.algorithms import bipartite
 from networkx.generators.random_graphs import erdos_renyi_graph
 import copy
-
+import scipy
 
 # -- Initialize graphs
 seed = 30
 G = nx.florentine_families_graph()
 nodes = G.nodes()
-
+A = nx.adjacency_matrix(G).todense()
 layout = nx.spring_layout(G, seed=seed)
 
 # -- compute jaccard's similarity
-"""
-    This example is using NetwrokX's native implementation to compute similarities.
-    Write a code to compute Jaccard's similarity and replace with this function.
-"""
-pred = nx.jaccard_coefficient(G)
 
+d =  list(dict(G.degree()).values())
+J_num = A * A  #walk of 2
+
+S = np.zeros(A.shape)
+for i in range (len(d)):
+    for j in range (len(d)):
+
+        S[i,j] = J_num[i,j]/(d[i]+d[j] - J_num[i,j])  # Similarity matrix
 
 # -- keep a copy of edges in the graph
 old_edges = copy.deepcopy(G.edges())
 
-# -- add new edges representing similarities.
-new_edges, metric = [], []
-for u, v, p in pred:
-    G.add_edge(u, v)
-    print(f"({u}, {v}) -> {p:.8f}")
-    new_edges.append((u, v))
-    metric.append(p)
 
-# -- plot Florentine Families graph
+new_edges, metric = [], []
+new_edges_calc, metric_calc = [], []
+
+node_names = list(G.nodes)
+
+# -- add new edges representing similarities.
+idx = -1
+for u in node_names:
+    idx = idx + 1
+    if (u == "Ginori" ):
+        idx_gio = idx
+    if (u != "Ginori" ):
+        new_edges_calc.append((u, "Ginori"))
+
+metric_calc.append(S[idx_gio]) # giving values of index from similarity matrix
+
+metric_calc = np.delete(metric_calc,idx_gio)
+
+from pandas import *
+print (DataFrame(S))
+
 nx.draw_networkx_nodes(G, nodelist=nodes, label=nodes, pos=layout, node_size=600)
 nx.draw_networkx_edges(G, edgelist=old_edges, pos=layout, edge_color='gray', width=4)
 
-# -- plot edges representing similarity
-"""
-    This example is randomly plotting similarities between 8 pairs of nodes in the graph. 
-    Identify the ”Ginori”
-"""
-ne = nx.draw_networkx_edges(G, edgelist=new_edges[:8], pos=layout, edge_color=np.asarray(metric[:8]), width=4, alpha=0.7)
+ne = nx.draw_networkx_edges(G, edgelist=new_edges_calc, pos=layout, edge_color=np.asarray(metric_calc), width=4, alpha=0.7, label = True)
 plt.colorbar(ne)
 plt.axis('off')
 plt.show()
+plt.savefig("From_calculation.png")
+
+####### using networkx ##########
+
+# pred = nx.jaccard_coefficient(G)
+
+# for u, v, p in pred:
+#     if (u == "Ginori" or v=="Ginori"):
+#        # G.add_edge(u, v)
+#         print(f"({u}, {v}) -> {p:.8f}")
+#         new_edges.append((u, v))
+#         metric.append(p)
+
+  
+
+# # -- plot Florentine Families graph
+# nx.draw_networkx_nodes(G, nodelist=nodes, label=nodes, pos=layout, node_size=600)
+# nx.draw_networkx_edges(G, edgelist=old_edges, pos=layout, edge_color='gray', width=4)
+
+# # -- plot edges representing similarity
+
+# ne = nx.draw_networkx_edges(G, edgelist=new_edges, pos=layout, edge_color=np.asarray(metric), width=4, alpha=0.7)
+# plt.colorbar(ne)
+# plt.axis('off')
+# plt.show()
+# plt.savefig("From_networkx.png")
+# plt.close()
+
+
+print("Finished")
