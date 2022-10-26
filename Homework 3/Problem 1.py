66be25047c5fc304da567234304399130aa7b208
nshervt <navidst@colorado.edu>
2022-10-25 23:23:44 -0400

    ENH: starter code for Problem 1 - HW3

---
+++ Homework 3/Problem 1.py
@@ -0,0 +1,104 @@
+import torch
+from torch import nn
+import warnings
+import numpy as np
+
+warnings.simplefilter(action='ignore', category=UserWarning)
+from chainer_chemistry import datasets
+from chainer_chemistry.dataset.preprocessors.ggnn_preprocessor import GGNNPreprocessor
+
+
+"""
+    load data
+"""
+dataset, dataset_smiles = datasets.get_qm9(GGNNPreprocessor(kekulize=True), return_smiles=True,
+                                           target_index=np.random.choice(range(133000), 100, False))
+
+V = 9
+atom_types = [6, 8, 7, 9, 1]
+
+def adj(x):
+    x = x[1]
+    adjacency = np.zeros((V, V)).astype(float)
+    adjacency[:len(x[0]), :len(x[0])] = x[0] + 2 * x[1] + 3 * x[2]
+    return torch.tensor(adjacency)
+
+
+def sig(x):
+    x = x[0]
+    atoms = np.ones((V)).astype(float)
+    out = np.array([int(atom == atom_type) for atom_type in atom_types for atom in atoms]).astype(float)
+    return torch.tensor(out).reshape(5, len(atoms)).T
+
+
+def target(x):
+    x = x[2]
+    return torch.tensor(x)
+
+
+adjs = torch.stack(list(map(adj, dataset)))
+sigs = torch.stack(list(map(sig, dataset)))
+prop = torch.stack(list(map(target, dataset)))[:, 5]
+
+
+class GCN:
+    """
+        Graph convolutional layer
+    """
+    def __init__(self, in_features, out_features):
+        # -- initialize weight
+        pass
+
+        # -- non-linearity
+
+    def __call__(self, A, H):
+        # -- GCN propagation rule
+        pass
+
+
+class GraphPooling:
+    """
+        Graph pooling layer
+    """
+    def __init__(self):
+        pass
+
+    def __call__(self, H):
+        # -- multi-set pooling operator
+        pass
+
+
+class MyModel(nn.Module):
+    """
+        Regression  model
+    """
+    def __init__(self):
+        super(MyModel, self).__init__()
+        # -- initialize layers
+        pass
+
+    def forward(self, A, h0):
+        pass
+
+
+"""
+    Train
+"""
+# -- Initialize the model, loss function, and the optimizer
+model = MyModel()
+# MyLoss =
+# MyOptimizer =
+
+# -- update parameters
+for epoch in range(200):
+    for i in range(10):
+
+        # -- predict
+        pred = model(adjs[i*10:(i+1)*10], sigs[i*10:(i+1)*10])
+
+        # -- loss
+        # loss = ?
+
+        # -- optimize
+
+# -- plot loss
