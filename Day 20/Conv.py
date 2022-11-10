3eb8dc64fe2ddbeb7674f8f7c7c314f5e71b80a3
nshervt <navidst@colorado.edu>
2022-11-10 16:11:24 -0500

    ENH: kernel defined

--- Day 20/Conv.py
+++ Day 20/Conv.py
@@ -1,12 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# -- 1D conv
+"""
+    1D conv
+"""
 n = 1000
 t = np.linspace(0, 1, n)
-y = np.sin(10 * t)
+y = np.sin(10 * t) + 0.3 * np.random.randn(n)
 
 plt.plot(t, y)
 plt.show()
 
-# -- 2D conv
+# -- define filter
+t_ = np.arange(-400, 400)
+sigma = 60
+k = np.exp(-t_**2/(2*sigma**2))
+k = k/np.sum(k)
+
+plt.plot(t_, k)
+plt.show()
+
+
+"""
+    2D conv
+"""
