c2422e3304d08dd4ee50a05529b15d94fff8be01
nshervt <navidst@colorado.edu>
2022-11-10 16:17:21 -0500

    ENH: convolve 1d

--- Day 20/Conv.py
+++ Day 20/Conv.py
@@ -9,7 +9,8 @@ t = np.linspace(0, 1, n)
 y = np.sin(10 * t) + 0.3 * np.random.randn(n)
 
 plt.plot(t, y)
-plt.show()
+# plt.show()
+# plt.close()
 
 # -- define filter
 t_ = np.arange(-400, 400)
@@ -17,9 +18,18 @@ sigma = 60
 k = np.exp(-t_**2/(2*sigma**2))
 k = k/np.sum(k)
 
+
+
+y_conv = np.convolve(y, k, mode='same')
+
+plt.plot(t, y_conv)
+plt.xlim(0, 1.)
+plt.show()
+plt.close()
+
 plt.plot(t_, k)
 plt.show()
-
+plt.close()
 
 """
     2D conv
