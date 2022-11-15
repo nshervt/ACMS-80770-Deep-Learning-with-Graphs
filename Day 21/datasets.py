c21adf07f49e88027a046ba7fbfdf85b9f009f6c
nshervt <navidst@colorado.edu>
2022-11-15 16:02:38 -0500

    ENH: vis FashionMNIST

--- Day 21/datasets.py
+++ Day 21/datasets.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 
 
 train_data = datasets.FashionMNIST(root='data', train=True,
-                                   download=True, transform=ToTensor())
+                                   download=False, transform=ToTensor())
 
 test_data = datasets.FashionMNIST(root='data', train=False,
-                                   download=True, transform=ToTensor())
+                                   download=False, transform=ToTensor())
+
+print(len(train_data))
+print(len(train_data[0]))
+print(train_data.data.shape)
+
+plt.figure()
+plt.imshow(train_data.data[0])
+plt.show()
