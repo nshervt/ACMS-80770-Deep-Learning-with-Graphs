fa26cfd0dd602774cec9163b975cc373635345fa
nshervt <navidst@colorado.edu>
2022-11-15 15:53:52 -0500

    ENH: dataset load

---
+++ Day 21/datasets.py
@@ -0,0 +1,11 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+
+train_data = datasets.FashionMNIST(root='data', train=True,
+                                   download=True, transform=ToTensor())
+
+test_data = datasets.FashionMNIST(root='data', train=False,
+                                   download=True, transform=ToTensor())
