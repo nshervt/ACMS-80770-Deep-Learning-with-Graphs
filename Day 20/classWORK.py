81c06e3356bc04984236727b59468f8ecec74076
nshervt <30446938+nshervt@users.noreply.github.com>
2022-12-12 14:39:53 -0500

    Delete Day 20 directory

--- Day 20/classWORK.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
-
-train_data = datasets.FashionMNIST(root = 'data', train=True, download = True, transform = ToTensor())
-
-test_data = datasets.FashionMNIST(root = 'data', train=False, download = True, transform = ToTensor())
